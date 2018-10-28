Fix normalization

dataset.py
@@ -3,21 +3,21 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import imageio
 imageio.plugins.freeimage.download()
-from torchvision import transforms, utils
-
+from torchvision import transforms, datasets
 from utility import *
 
 #Note1: Pillow does not support RGB-32bit images
 
 class LdrHdrDataset(Dataset):
-    def __init__(self, data_dir, is_train):
+    def __init__(self, data_dir, is_train, num_batch):
         self.is_train = is_train
+        self.num_batch = num_batch
 
-        #Read images
+        #Read HDR images
         assert os.path.exists(data_dir)
         filenames = glob.glob('{}/*.hdr'.format(data_dir))
 
-        #Load images on memory
+        #Load HDR images on memory
         self.hdr_images = []
         for filename in filenames:
             hdr_image = imageio.imread(filename, format='HDR-FI')
@@ -26,26 +26,37 @@ class LdrHdrDataset(Dataset):
         #Transforms
         if self.is_train:
             #Train: Crop, Clip, Normalization, Camera curve, Rotate
-            self.composed = transforms.Compose([RandomCrop((320, 320)),
+            self.composed_ldr = transforms.Compose([RandomCrop((320, 320)),
+                                            RandomRotate(),
                                             Clip(),
                                             Normalize((320, 320)),
                                             CameraCurve(),
-                                            RandomRotate(),
                                             ToTensor()])
         else:
             #Test: Clip, Normalization, Camera curve
-            self.composed = transforms.Compose([Clip(),
-                                            Pad(),
+            self.composed_ldr = transforms.Compose([Pad(),
+                                            Clip(),
                                             Normalize((1920, 1080)),
                                             CameraCurve(),
                                             ToTensor()])
 
     def __len__(self):
-        return len(self.hdr_images)
+        if self.is_train:
+            return self.num_batch * 1000
+        else:
+            return len(self.hdr_images)
 
-    def __getitem__(self, idx):
-        hdr_img = self.hdr_images[idx]
-        ldr_img = self.composed(hdr_img)
+    def __getitem__(self, idx=None):
+        if self.is_train:
+            idx = random.randint(0, len(self.hdr_images) - 1)
+            img  = self.hdr_images[idx]
+
+            ldr_img, hdr_img = self.composed_ldr(img)
+        else:
+            assert idx != None
+            img = self.hdr_images[idx]
+
+            ldr_img, hdr_img = self.composed_ldr(img)
 
         return ldr_img, hdr_img
 

model.py
@@ -31,13 +31,6 @@ class Skip_Conv(nn.Module):
         self.conv = nn.Conv2d(in_channels=num_features_in * 2, out_channels=num_features_out, kernel_size=1, stride=1, padding=0, bias=bias)
 
         #Weight initialization: TODO
-        """
-        conv_weights = np.zeros((num_features, num_features * 2, 1, 1))
-        for idx in range(num_features):
-            conv_weights[idx, idx, 0, 0] = 1
-            conv_weights[idx, idx * 2, 0, 0] = 1
-        self.conv.weight.data = torch.from_numpy(conv_weights)
-        """
 
     def forward(self, x_input, x_skip):
         #Process a skip connection output
@@ -119,7 +112,7 @@ class UNet(nn.Module):
         self.decoder.append(nn.ModuleList().extend((decode_5_1, decode_5_2)))
         self.decoder.append(decode_6_1)
 
-    def forward(self, x):
+    def forward(self, x, alpha):
         #LDR encoder
         input_image = x
         x = self.encoder[0](x)
@@ -151,9 +144,6 @@ class UNet(nn.Module):
         x = self.decoder[5](x, input_image)
 
         #Final
-        max_input, _ = torch.max(input_image, dim=1)
-        alpha = torch.clamp(torch.clamp(max_input - 0.95, min=0) / 0.05, max=1)
-        alpha = torch.stack((alpha, alpha, alpha), dim=1)
         output = torch.mul(1-alpha, torch.pow(input_image, 2)) + torch.mul(alpha, torch.exp(x))
 
         return output
@@ -166,4 +156,3 @@ if __name__ == "__main__":
 
     output = model(tensor)
     print(output.size())
-

utility.py
@@ -1,5 +1,6 @@
+import torch
 import numpy as np
-import random
+import random, logging, os
 
 HDR_MIN = 0
 HDR_MAX = 100000
@@ -31,9 +32,25 @@ class RandomCrop(object):
 
         return image
 
+class RandomRotate(object):
+    def __call__(self, image):
+
+        for _ in range(random.randint(1,3)):
+            image = np.rot90(image)
+
+        if random.randint(0,1):
+            image = np.flip(image, 0)
+
+            image = np.flip(image, 1)
+
+        return image
+
 class Clip(object):
     def __call__(self, image):
-        return np.clip(image, HDR_MIN, HDR_MAX)
+
+        image = np.clip(image, a_min=HDR_MIN, a_max=HDR_MAX)
+
+        return image
 
 class Normalize(object):
     def __init__(self, image_size):
@@ -41,43 +58,61 @@ class Normalize(object):
         self.num_pixels = image_size[0] * image_size[1]
 
     def __call__(self, image):
-        image_1d = image.flatten()
-        image_1d.sort(axis=0)
+        image_mean = np.mean(image, axis=2)
+        image_1d = image_mean.flatten()
 
         rand_idx = np.random.randint(self.num_pixels * NORMALIZE_MIN, self.num_pixels * NORMALIZE_MAX)
         normalized_value = image_1d[-rand_idx]
+        image = image / normalized_value
 
-        return image / normalized_value
+        return image
 
 class CameraCurve(object):
     def __call__(self, image):
-        n = np.normal(N_MEAN, N_VAR)
-        sig = np.normal(SIG_MEAN, SIG_VAR)
+        target = image #target (x Camera curve)
 
-        image = np.power(image, n)
-        image = (1 + sig) * (image / image + sig)
+        n = np.clip(np.random.normal(N_MEAN, N_VAR), a_min=0, a_max=2.5)
+        sig = np.clip(np.random.normal(SIG_MEAN, SIG_VAR), a_min=0, a_max=5)
 
-        return image
+        input = np.power(image, n)
+        input = (1 + sig) * (input / (input + sig)) #input (o Camera curve)
 
-class RandomRotate(object):
-    def __call__(self, image):
-        image = image.rotate(90 * random.randint(1,3))
-
-        if random.randint(0,1):
-            image = np.flip(image, 0)
-
-        if random.randint(0,1):
-            image = np.flip(image, 1)
-
-        return image
+        return (input, target)
 
 class Pad(object):
     def __call__(self, image):
-        image = np.pad(image, ((0,0), (4,4), (0, 0)), 'edge')
+        image = np.pad(image, ((4,4), (0,0), (0, 0)), 'edge')
 
         return image
 
 class ToTensor(object):
-    def __call__(self, image):
-        image = image.transpose((2, 0, 1))
-        return torch.from_numpy(image)
+    def __call__(self, images):
+        input = images[0]
+        target = images[1]
+
+        #input (Clip & Quantization)
+        input = np.floor((np.clip(input, a_min=0, a_max=1) * 255 + 0.5)) / 255
+
+        input = input.transpose((2, 0, 1))
+        input = torch.from_numpy(input.copy())
+
+        target = target.transpose((2, 0, 1))
+        target = torch.from_numpy(target.copy())
+
+        return (input, target)
+
+def getLogger(save_dir, save_name):
+    Logger = logging.getLogger(save_name)
+    Logger.setLevel(logging.INFO)
+    Logger.propagate = False
+
+    filePath = os.path.join(save_dir, save_name)
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
+    fileHandler = logging.FileHandler(filePath)
+    logFormatter = logging.Formatter('%(message)s')
+    fileHandler.setFormatter(logFormatter)
+    Logger.addHandler(fileHandler)
+
+    return Logger